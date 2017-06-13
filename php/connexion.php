64db39552fb1dff3fecd240a23205215ddbe3a31
Unknown <clouet.marie@gmail.com>
2017-06-13 16:14:12 +0200

    Connexion DANOU BALI
    oui session hey cool

--- php/connexion.php
+++ php/connexion.php
@@ -1,5 +1,6 @@
 <?php
     header("Content-type: text/html");
+    session_start();
 ?>
 
     <!DOCTYPE html>
@@ -61,51 +62,84 @@
 
         <!-- ---------------------- PHP ----------------- -->
 
+        <?php
+            if(!isset($_SESSION["pseudoCo"])) { // Si la session n'existe pas = si l'utilisateur n'est pas connecté
+                echo("salut");
+                if (isset($_POST["pseudoCo"])) { // Si le formulaire a été envoyé
+                    $pseudoCo = addslashes($_POST["pseudoCo"]);
+                    $motDePasseCo = addslashes($_POST["motDePasseCo"]);
+                    
+                    
+                    try {
+                        // Etape 1 : connexion au serveur de base de données
+                        require("param.inc.php");
+                        $pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
+                        $pdo->query("SET NAMES utf8");
+                        $pdo->query("SET CHARACTER SET 'utf8'");
 
-        <!-- ---------------------- FIN PHP ----------------- -->
+                        // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER PSEUDO ET MOT DE PASSE
+                        $sql = "SELECT Pseudo, MotDePasse FROM UTILISATEUR WHERE Pseudo = '".$pseudoCo."'";
+                        $statement = $pdo->query($sql);
 
-        <main>
-            <h2 class="titreBleu">Connexion</h2>
+                        // Etape 3 : traitement des données retournées
+                        $ligne = $statement->fetch(PDO::FETCH_ASSOC);
+                        
+                        if ($motDePasseCo == $ligne["MotDePasse"] and $pseudoCo == $ligne["Pseudo"]) {
+                            echo("hey cool");
+                            $form = false;
+                            $_SESSION["pseudoCo"] = $pseudoCo;
+                        }
+                        
+                        $pdo = null;
+                    } catch(Exception $e) {
+                        echo("Exception :".$e->getMessage());
+                    }
 
-            <form action="connexion.php" id="connexion" method="post" class="formulaire">
-                <div>
-                    <label for="pseudoCo">Pseudo</label>
-                    <input id="pseudoCo" type="text" name="pseudoCo" required="required" />
+                } // Fin condition si le formulaire a été envoyé
+            } else {// Si la session existe = si l'utilisateur est connecté
+                echo("d'accord");
+            }
+        ?>
+
+
+            <!-- ---------------------- FIN PHP ----------------- -->
+
+            <main>
+                <h2 class="titreBleu">Connexion</h2>
+
+                <form action="connexion.php" id="connexion" method="post" class="formulaire">
+                    <div>
+                        <label for="pseudoCo">Pseudo</label>
+                        <input id="pseudoCo" type="text" name="pseudoCo" required="required" />
+                    </div>
+
+                    <div>
+                        <label for="motDePasseCo">Mot de passe</label>
+                        <input type="password" name="motDePasseCo" id="motDePasseCo" required="required" pattern=".{6,}" />
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Se connecter" class="inputSubmit" />
+                    </div>
+                </form>
+
+            </main>
+
+            <footer>
+                <div class="txtFooter">
+                    <p><a href="../mentions.html">Mentions légales</a></p>
+                    <p><a href="../contact.html">Formulaire de contact</a></p>
+                    <p><a href="../modalites.html">Modalités du concours</a></p>
+                    <p><a href="../sponsors.html">Sponsors</a></p>
                 </div>
-
                 <div>
-                    <label for="emailCo">E-mail</label>
-                    <input type="text" name="emailCo" id="emailCo" required="required" />
+                    <a href="https://www.facebook.com/Cest-dans-le-sac-1703344363292608/" target="_blank"><img src="../images/img-facebook-icon.png" alt="logo Facebook" /></a>
+                    <a href="https://twitter.com/cestdanslesac" target="_blank"><img src="../images/img-tweeter-icon.png" alt="logo Twitter" /></a>
+                    <a href="https://fr.pinterest.com/cdanslesac/" target="_blank"><img src="../images/img-pinterest-icon.png" alt="logo Pinterest" /></a>
+                    <a href="https://www.instagram.com/cestdanslesac/" target="_blank"><img src="../images/img-instagram-icon.png" alt="logo Instagram" /></a>
+                    <a href="https://plus.google.com/108664054375147502962" target="_blank"><img src="../images/img-googleplus-icon.png" alt="logo Google+" /></a>
                 </div>
-
-                <div>
-                    <label for="motDePasseCo">Mot de passe</label>
-                    <input type="password" name="motDePasseCo" id="motDePasseCo" required="required" pattern=".{6,}" />
-                </div>
-
-                <div>
-                    <input type="submit" value="Se connecter" class="inputSubmit" />
-                </div>
-            </form>
-
-        </main>
-
-        <footer>
-            <div class="txtFooter">
-                <p><a href="../mentions.html">Mentions légales</a></p>
-                <p><a href="../contact.html">Formulaire de contact</a></p>
-                <p><a href="../modalites.html">Modalités du concours</a></p>
-                <p><a href="../sponsors.html">Sponsors</a></p>
-            </div>
-            <div>
-                <a href="https://www.facebook.com/Cest-dans-le-sac-1703344363292608/" target="_blank"><img src="../images/img-facebook-icon.png" alt="logo Facebook" /></a>
-                <a href="https://twitter.com/cestdanslesac" target="_blank"><img src="../images/img-tweeter-icon.png" alt="logo Twitter" /></a>
-                <a href="https://fr.pinterest.com/cdanslesac/" target="_blank"><img src="../images/img-pinterest-icon.png" alt="logo Pinterest" /></a>
-                <a href="https://www.instagram.com/cestdanslesac/" target="_blank"><img src="../images/img-instagram-icon.png" alt="logo Instagram" /></a>
-                <a href="https://plus.google.com/108664054375147502962" target="_blank"><img src="../images/img-googleplus-icon.png" alt="logo Google+" /></a>
-            </div>
-        </footer>
-        <script type="text/javascript" src="../js/inscription.js"></script>
+            </footer>
     </body>
 
     </html>
--- php/inscription.php
+++ php/inscription.php
@@ -1,5 +1,6 @@
 <?php
     header("Content-type: text/html");
+    session_start();
 ?>
 
 <!DOCTYPE html>

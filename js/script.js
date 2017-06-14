027837b4b16a9a7edcb7836626515ad43a3a4774
Unknown <clouet.marie@gmail.com>
2017-06-14 16:35:39 +0200

    Nav hover et actif ok

--- js/script.js
+++ js/script.js
@@ -6,20 +6,23 @@ function initialiser(evt) {
     var couleurs = new Array("#eebd30", "#91dfd6", "#a1cd69", "#fe5d99", "#ffbad4");
     var nbCouleurs = couleurs.length;
     var actif = document.getElementsByClassName("actif");
+    var nav = document.querySelectorAll("nav a");
 
+    for (var unNav of nav) {
+        unNav.addEventListener("mouseenter", changerCouleur);
+        unNav.addEventListener("mouseleave", supprimerCouleur);
+    }
 
     // Choix de la couleur aléatoire
     var laCouleur = Math.round(Math.random() * nbCouleurs);
 
     for (var unActif of actif) {
         unActif.style.color = couleurs[laCouleur];
+        unActif.removeEventListener("mouseenter", changerCouleur);
+        unActif.removeEventListener("mouseleave", supprimerCouleur);
     }
 
-    var nav = document.querySelectorAll("nav a");
-    for (var unNav of nav) {
-        unNav.addEventListener("mouseenter", changerCouleur);
-        unNav.addEventListener("mouseleave", supprimerCouleur);
-    }
+
 }
 
 function changerCouleur(evt) {
@@ -28,19 +31,19 @@ function changerCouleur(evt) {
     var laCouleur = Math.round(Math.random() * nbCouleurs);
     var actif = document.getElementsByClassName("actif");
 
-    for (var unActif of actif) {
-        if (this != unActif) {
-            this.style.color = couleurs[laCouleur];
-        }
-    }
+    //for (var unActif of actif) {
+    //if (this != unActif) {
+    this.style.color = couleurs[laCouleur];
+    //}
+    //}
 }
 
 function supprimerCouleur(evt) {
     var actif = document.getElementsByClassName("actif");
 
-    for (var unActif of actif) {
-        if (this != unActif) {
-            this.style.color = "#000000";
-        }
-    }
+    //for (var unActif of actif) {
+    //if (this != unActif) {
+    this.style.color = "#000000";
+    //}
+    //}
 }

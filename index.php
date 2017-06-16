0daff151453572b122be8dea4bfd9ca117103685
a a <leasaimond@gmail.com>
2017-06-16 09:14:26 +0200

    index balise br lol

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
         <h2 class="titreVert">Concours Cabadix</h2>
         <div id="presentationSite">
             <p>
-                Bonjour et bienvenue !
+                Bonjour et bienvenue !</br>
                 Ici, nous avons à coeur la protection de la planète et nous organisons un concours pour lutter contre un fléau encore trop présent : les sacs plastiques.
                 <br/>Vous pourrez libérer votre créativité au travers d'affiches, de clips audio et de vidéos.
                 <br/>Les 10 gagnants se verront remettre des lots, dont des sacs CABADIX !

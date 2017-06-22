394c07c5f8b80b85c92df0495a6be6bef4d2195c
Unknown <clouet.marie@gmail.com>
2017-06-22 14:48:53 +0200

    Gestion css afficheImage + UPDATE != INSERT INTO

--- afficheImage.php
+++ afficheImage.php
@@ -73,14 +73,15 @@
         <main> 
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
 <?php
-           //ETAPE 1: connexion à la base de données
+            // AFFICHER TITRE, OEUVRE, AUTEUR, BIODRAPHIE, DESCRITPION
+            //ETAPE 1: connexion à la base de données
             require("param.inc.php");
             $pdo = new PDO("mysql:host=".MYHOST.";dbname=".MYDB,MYUSER,MYPASS);
             $pdo -> query("SET NAMES utf8");
             $pdo -> query("SET CHARACTER SET 'utf8'");  
 
             //ETAPE 2: Envoyer une requête SQL
-            if(isset ($_GET["idImg"])){ //si l'idOeuvre a été passée par l'URL
+            if(isset ($_GET["idImg"])){ // Si l'idOeuvre a été passée par l'URL
                 
                 $idOeuvre = $_GET["idImg"];
                 
@@ -115,7 +116,7 @@
               
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
 <?php
-                if($ligne["Type"] == "affiche"){
+                if($ligne["Type"] == "affiche") {
                     
 ?>
     <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
@@ -124,7 +125,7 @@
                     </div>
     <!-- - - - - - - - - - PHP - - - - - - - - - -->            
 <?php
-                }else if($ligne["Type"] == "video"){
+                } else if($ligne["Type"] == "video") {
 ?>
     <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->    
                     <div>
@@ -134,7 +135,7 @@
                     </div>
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
 <?php
-                }else{
+                } else {
 ?>
     <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
                     <div>
@@ -143,43 +144,55 @@
                     </div>
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
 <?php
-                } //fin if type
-            } else { //si idImg n'existe pas
+                } // Fin if type
+                
+    
+            } else { // Si idOeuvre n'existe pas
                 header("Location: ./galerie.php");
-            }            
+            }
+
+            // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER NOTE
+            $sql = "SELECT Note, Pseudo, IdOeuvre FROM NOTE WHERE IdOeuvre = '".$idOeuvre."' AND Pseudo = '".$_SESSION["pseudoCo"]."'";
+
+            $statement = $pdo->query($sql);
+
+            // Etape 3 : traitement des données retournées
+            $ligneNote = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if($ligneNote != false) { // Si l'utilisateur a déjà noté cette oeuvre
+                if($ligneNote["Note"] == '0') { // Si il a supprimé son vote
+                    $srcBtn = "./images/img-bouton-voter.png";
+                } else { // Si son vote pour cette oeuvre est à 1
+                    $srcBtn = "./images/img-bouton-favori.png";
+                }
+            } else { // Si l'utilisateur n'a encore jamais noté cette oeuvre
+                $srcBtn = "./images/img-bouton-voter.png";
+            } // Fin condition si l'utilisateur a déjà noté cette oeuvre
+
 ?>
     <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
             <div id="descNote">
                 <div id="descAut">
                     <h3>Description par l'auteur</h3>
                     <p class="descOeuvre">
-                        <?php
-                        echo($ligne["DescOeuvre"]);
-                        ?>
+                        <?php echo($ligne["DescOeuvre"]); ?>
                     </p>
                 </div>
                 <form action="afficheImage.php?idImg=<?php echo($idOeuvre) ?>" method="post">
-                <button type="submit" name="noterOeuvre">
-                    <img src="./images/img-bouton-ajouter.png" alt="Bouton de notation de l'oeuvre" />
-                </button>
+                    <button type="submit" name="noterOeuvre">
+                        <img src="<?php echo($srcBtn); ?>" alt="Bouton de notation de l'oeuvre" />
+                    </button>
                 </form>
                 
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
 <?php
-            $pdo = null;
-                      
-        // Etape 1 : connexion au serveur de base de données
-            require("param.inc.php");
-            $pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
-            $pdo->query("SET NAMES utf8");
-            $pdo->query("SET CHARACTER SET 'utf8'");
 
         // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER TITRE
-            $sql = "SELECT COUNT(Note) FROM NOTE WHERE IdOeuvre = '".$idOeuvre."'";
-            $statement = $pdo->query($sql);     
+        $sql = "SELECT COUNT(Note) FROM NOTE WHERE IdOeuvre = '".$idOeuvre."'";
+        $statement = $pdo->query($sql);     
 
         // Etape 3 : traitement des données retournées
-            $ligne = $statement->fetch(PDO::FETCH_ASSOC);
+        $ligne = $statement->fetch(PDO::FETCH_ASSOC);
             
 //            echo("$ligne 'countnote   '".$ligne["COUNT(Note)"]);
 ?>
@@ -192,7 +205,9 @@
 <?php
             $pdo = null;
         
-            if($admin == "oui") { // Affichage du bouton de suppression de l'oeuvre si l'utilisateur est admin
+            if(isset($_SESSION["pseudoCo"])) { // Si l'utilisateur est connecté
+        
+                if($admin == "oui") { // Affichage du bouton de suppression de l'oeuvre si l'utilisateur est admin
 ?>
     <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
         <form action="afficheImage.php?idImg=<?php echo($idOeuvre) ?>" method="post" id="supprOeAdmin">
@@ -203,106 +218,147 @@
         
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
     <?php   
-                if(isset($_POST["adminSupprOe"])) { // Si l'utilisateur clique sur le bouton de suppression de l'oeuvre
-                    
-                    // Etape 1 : connexion au serveur de base de données
-                    require("param.inc.php");
-                    $pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
-                    $pdo->query("SET NAMES utf8");
-                    $pdo->query("SET CHARACTER SET 'utf8'");
+                
+                    if(isset($_POST["adminSupprOe"])) { // Si l'utilisateur clique sur le bouton de suppression de l'oeuvre
 
-                    // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER TITRE
-                    $sql = "SELECT Pseudo, Titre, GdeOeuvre, Vignette, Type FROM OEUVRE WHERE IdOeuvre = '".$idOeuvre."'";
-                    $statement = $pdo->query($sql);
-
-                    // Etape 3 : traitement des données retournées
-                    $ligne = $statement->fetch(PDO::FETCH_ASSOC);
-
-                    $auteur = $ligne["Pseudo"];
-                    
-                    if($ligne["Type"] == "affiche") { // Si l'oeuvre est une affiche
-                        unlink("./php/images/".$ligne["GdeOeuvre"]);
-                        unlink("./php/vignettes/".$ligne["Vignette"]);
-                        $type = "L'affiche";
-                        $suppr = "supprimée";
-                    } else if($ligne["Type"] == "video") { // Si l'oeuvre est une vidéo
-                        unlink("./php/videos/".$ligne["GdeOeuvre"]);
-                        unlink("./php/vignettes/".$ligne["Vignette"]);
-                        $type = "La vidéo";
-                        $suppr = "supprimée";
-                    } else if($ligne["Type"] == "audio") { // Si l'oeuvre est un clip audio
-                        unlink("./php/clips-audio/".$ligne["GdeOeuvre"]);
-                        unlink("./php/vignettes/".$ligne["Vignette"]);
-                        $type = "Le clip audio";
-                        $suppr = "supprimé";
-                    } // Fin condition type oeuvre
-
-                    try {
                         // Etape 1 : connexion au serveur de base de données
                         require("param.inc.php");
                         $pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
                         $pdo->query("SET NAMES utf8");
                         $pdo->query("SET CHARACTER SET 'utf8'");
 
-                        // Etape 2 : envoi de la requête SQL au serveur SUPPRIMER TOUT
-                        $sql = "DELETE FROM OEUVRE WHERE IdOeuvre = '".$idOeuvre."'";
+                        // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER TITRE
+                        $sql = "SELECT Pseudo, Titre, GdeOeuvre, Vignette, Type FROM OEUVRE WHERE IdOeuvre = '".$idOeuvre."'";
                         $statement = $pdo->query($sql);
 
                         // Etape 3 : traitement des données retournées
                         $ligne = $statement->fetch(PDO::FETCH_ASSOC);
-                    } catch(Exception $e) {
-                        echo("Exception :".$e->getMessage());
-                    }
-                    
-                    $pdo = null;
+
+                        $auteur = $ligne["Pseudo"];
+
+                        if($ligne["Type"] == "affiche") { // Si l'oeuvre est une affiche
+                            unlink("./php/images/".$ligne["GdeOeuvre"]);
+                            unlink("./php/vignettes/".$ligne["Vignette"]);
+                            $type = "L'affiche";
+                            $suppr = "supprimée";
+                        } else if($ligne["Type"] == "video") { // Si l'oeuvre est une vidéo
+                            unlink("./php/videos/".$ligne["GdeOeuvre"]);
+                            unlink("./php/vignettes/".$ligne["Vignette"]);
+                            $type = "La vidéo";
+                            $suppr = "supprimée";
+                        } else if($ligne["Type"] == "audio") { // Si l'oeuvre est un clip audio
+                            unlink("./php/clips-audio/".$ligne["GdeOeuvre"]);
+                            unlink("./php/vignettes/".$ligne["Vignette"]);
+                            $type = "Le clip audio";
+                            $suppr = "supprimé";
+                        } // Fin condition type oeuvre
+
+                        try {
+                            // Etape 2 : envoi de la requête SQL au serveur SUPPRIMER TOUT
+                            $sql = "DELETE FROM OEUVRE WHERE IdOeuvre = '".$idOeuvre."'";
+                            $statement = $pdo->query($sql);
+
+                            // Etape 3 : traitement des données retournées
+                            $ligne = $statement->fetch(PDO::FETCH_ASSOC);
+                        } catch(Exception $e) {
+                            echo("Exception :".$e->getMessage());
+                        }
+
+                        $pdo = null;
     ?>
     <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
                 <div class="flou visible">
                      <div class="popup visible">
                         <h3>Suppression</h3>
                         <p><?php echo($type); ?> de <?php echo($auteur); ?> a bien été <?php echo($suppr); ?></p>
-                        <button class="fermer" id="afficheImage">Fermer</button>
+                        <button class="fermer afficheImage">Fermer</button>
                     </div>
                 </div>
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
     <?php
 
-                } // Fin condition si l'utilisateur clique sur le bouton de suppression de l'oeuvre
-            } // Fin condition si l'utilisateur est un admin
-        
-            if(isset($_POST["noterOeuvre"])){
-                
-                // Etape 1 : connexion au serveur de base de données
+                    } // Fin condition si l'utilisateur clique sur le bouton de suppression de l'oeuvre
+                } // Fin condition si l'utilisateur est un admin
+
+                if(isset($_POST["noterOeuvre"])){ // Si l'utilisateur clique sur le bouton de vote
+
+                    // Etape 1 : connexion au serveur de base de données
                     require("param.inc.php");
                     $pdo=new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS) ;
                     $pdo->query("SET NAMES utf8");
                     $pdo->query("SET CHARACTER SET 'utf8'");
 
-                // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER OEUVRE
+                    // Etape 2 : envoi de la requête SQL au serveur SELECTIONNER OEUVRE
                     $sql = "SELECT Note, Pseudo, IdOeuvre FROM NOTE WHERE IdOeuvre = '".$idOeuvre."' AND Pseudo = '".$_SESSION["pseudoCo"]."'";
 
                     $statement = $pdo->query($sql);
 
-                // Etape 3 : traitement des données retournées
+                    // Etape 3 : traitement des données retournées
                     $ligne = $statement->fetch(PDO::FETCH_ASSOC);
-                    
-                    if($ligne != false){
-                        if($ligne["Note"] == '0'){
-                            $sql = "INSERT INTO NOTE (Pseudo, IdOeuvre, Note) VALUES ('".$_SESSION["pseudoCo"]."','".$idOeuvre."', '1')";
-            
-                            echo("la note était à 0");
+
+                    if($ligne != false) { // Si l'utilisateur a déjà noté cette oeuvre
+                        if($ligne["Note"] == '0') { // Si il a supprimé son vote
+                            $sql = "UPDATE NOTE SET Note = '1' WHERE Pseudo = '".$_SESSION["pseudoCo"]."' AND IdOeuvre = '".$idOeuvre."'";
                             $statement = $pdo->query($sql);
-                            
-                        }else{
-                            $sql = "INSERT INTO NOTE (Pseudo, IdOeuvre, Note) VALUES (".$_SESSION["pseudoCo"].",".$idOeuvre.", '0')";
+?>
+    <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
+                <div class="flou visible">
+                     <div class="popup visible">
+                        <h3>Bravo</h3>
+                        <p>Votre vote a bien été pris en compte !</p>
+                        <button class="fermer">Fermer</button>
+                    </div>
+                </div>
+    <!-- - - - - - - - - - PHP - - - - - - - - - -->
+<?php
+                        } else { // Si son vote pour cette oeuvre est à 1
+                            $sql = "UPDATE NOTE SET Note = '0' WHERE Pseudo = '".$_SESSION["pseudoCo"]."' AND IdOeuvre = '".$idOeuvre."'";
                             $statement = $pdo->query($sql);
+?>
+    <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
+                <div class="flou visible">
+                     <div class="popup visible">
+                        <h3>Bravo</h3>
+                        <p>Votre vote a bien été pris en compte !</p>
+                        <button class="fermer">Fermer</button>
+                    </div>
+                </div>
+    <!-- - - - - - - - - - PHP - - - - - - - - - -->
+<?php
                         }
-                    }else{
+                    } else { // Si l'utilisateur n'a encore jamais noté cette oeuvre
                         $sql = "INSERT INTO NOTE (Pseudo, IdOeuvre, Note) VALUES ('".$_SESSION["pseudoCo"]."','".$idOeuvre."', '1')";
                         $statement = $pdo->query($sql);
-                    }
+?>
+    <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
+                <div class="flou visible">
+                     <div class="popup visible">
+                        <h3>Bravo</h3>
+                        <p>Votre vote a bien été pris en compte !</p>
+                        <button class="fermer">Fermer</button>
+                    </div>
+                </div>
+    <!-- - - - - - - - - - PHP - - - - - - - - - -->
+<?php
+                    } // Fin condition si l'utilisateur a déjà noté cette oeuvre
+
+                    $pdo = null;
+                } // Fin condition si l'utilisateur clique sur le bouton de vote
                 
-                $pdo = null;
+            } else { // Si l'utilisateur n'est pas connecté
+                if(isset($_POST["noterOeuvre"])) {
+?>
+    <!-- - - - - - - - - - FIN PHP - - - - - - - - - -->
+                <div class="flou visible">
+                     <div class="popup visible">
+                        <h3>Erreur</h3>
+                        <p>Vous devez vous connecter pour pouvoir voter.</p>
+                        <button class="fermer">Fermer</button>
+                    </div>
+                </div>
+    <!-- - - - - - - - - - PHP - - - - - - - - - -->
+<?php
+                }
             }
         
             require("pied.inc.php");

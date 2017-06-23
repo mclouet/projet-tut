ecd049eb73921bba6b377109e8663291473f54f6
JPP <leasaimond@gmail.com>
2017-06-23 10:28:19 +0200

    CSS sponsors fini

--- sponsors.php
+++ sponsors.php
@@ -45,28 +45,22 @@
     </nav>
     <main id="sponsors">
         <h2 class="titreVert">Sponsors</h2>
-
-        <p>
-            Bienvenue sur le site C'est dans le sac !
-            <br/> Nous sommes un site concours pour la promotion de sacs respectueux de l'environnement, nous sommes à la recherches de sponsors pour porter notre projet.
-            <br/> En contrepartie d'une visibilité ou d'une annonce sur votre réseau, d'un soutient financier pour les lots ou si vous en avez à proposer, vous disposerez d'un espace sur le site et un lien direct vers votre site internet.
-            <br/> Afin de devenir partenaire, vous devez faire preuve ou avoir fais preuve d'intérêt envers le thème de la pollution, avoir un lot à proposer aux gagnants, un site internet (pour disposer d'un lien).
-            <br/> Des ajustements pourront être apportés à ses conditions dans la mesure du possible, une fois la collaboration mise en place. Pour déposer une demande pour devenir partenaire il vous faudra prendre contact avec Cabadix :
-            <br/> Si vous souhaitez devenir sponsor, n'hésitez pas à nous contacter.<span class="gras"><a href="contact.php">Plus de renseignements</a></span>
-
-            <ul>
-                <li>Adresse facebook : <a href="https://www.facebook.com/TrolleyBagsFR/" target="_blank">TrolleyBags</a>
-                </li>
-                <li>Adresse email : <a href="" target="_blank">service-cabadix@cabadix.com</a>
-                </li>
-            </ul>
-
-        </p>
-
-
+        <div>
+            <p>
+                Bienvenue sur le site C'est dans le sac !
+                <br/> Nous sommes un site concours pour la promotion de sacs respectueux de l'environnement, nous sommes à la recherches de sponsors pour porter notre projet.
+                <br/> En contrepartie d'une visibilité ou d'une annonce sur votre réseau, d'un soutient financier pour les lots ou si vous en avez à proposer, vous disposerez d'un espace sur le site et un lien direct vers votre site internet.
+                <br/> Afin de devenir partenaire, vous devez faire preuve ou avoir fait preuve d'intérêt envers le thème de la pollution, avoir un lot à proposer aux gagnants, un site internet (pour disposer d'un lien).
+                <br/> Des ajustements pourront être apportés à ces conditions dans la mesure du possible, une fois la collaboration mise en place.
+                <br/> Si vous souhaitez devenir sponsor, n'hésitez pas à nous contacter.
+            </p>
+        </div>
+        <div>
+            <a href="contact.php" class="titreBleu btnHover">Plus de renseignements</a>
+            <a href="https://packingsorted.fr/" class="titreRose btnHover" target="_blank">Visitez la boutique</a>
+        </div>
         <img src="./images/img-charte-cabadix.png" alt="Logo de Cabadix" />
-        <a href="https://packingsorted.fr/" target="_blank">packingsorted.fr</a>
-
+        
     </main>
     <!-- - - - - - - - - - PHP - - - - - - - - - -->
 <?php
